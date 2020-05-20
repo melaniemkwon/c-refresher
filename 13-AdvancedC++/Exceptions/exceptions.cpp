bca0ec963ef4e3daa1152fff752da5c44d23b3d4
melaniemkwon <melaniemkwon@gmail.com>
2020-05-20 20:40:27 +0000

    exceptions

---
+++ 13-AdvancedC++/Exceptions/exceptions.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+class DivisionByZeroException
+{
+    public:
+        void getErrorMessage() { cout << "Error: Do not divide by zero"; }
+};
+double division(double a, double b)
+{
+    if (b == 0)
+    {
+        throw DivisionByZeroException();
+    }
+    return a / b;
+}
+
+int main()
+{
+    // int a = 5;
+    char a = 5;
+
+    try
+    {
+        a *= 10;
+
+        if (a == 50)
+            throw a;
+
+        cout << "A is valid." << endl;
+    }
+    catch(int e)
+    {
+        cout << "A can't be " << a << endl;
+    }
+    catch (...)
+    {
+        cout << "Non-int catch " << endl;
+    }
+
+    cout << "----------------------" << endl;
+
+    try
+    {
+        double result = division(5, 0);
+        cout << "result is " << result << endl;
+    }
+    catch(DivisionByZeroException e)
+    {
+        e.getErrorMessage();
+    }
+
+
+    return 0;
+}
