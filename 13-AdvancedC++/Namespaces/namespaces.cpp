e50fbd36b300019c2bf051161fa06b7e841fc229
melaniemkwon <melaniemkwon@gmail.com>
2020-05-20 21:32:20 +0000

    custom namespaces

--- 13-AdvancedC++/Namespaces/namespaces.cpp
+++ 13-AdvancedC++/Namespaces/namespaces.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-using namespace std;
+// using namespace std;
 
 namespace A
 {
@@ -11,19 +11,41 @@ namespace B
 {
     int a = 99;
 }
+namespace mySpace
+{
+    class MyNewLine
+    {
+            std::string text;
+        public:
+            MyNewLine(std::string text = "\n\n\n") { this->text = text;}
+            std::string toString()
+            {
+                return text;
+            }
+    };
+
+    std::ostream & operator<<(std::ostream & out, MyNewLine & o)
+    {
+        return out << o.toString();
+    }
+    MyNewLine endl;
+}
 
 int a = 3;
 
 using namespace A;
+using namespace mySpace;
 int main()
 {
-    int a = 1;
-    cout << a << endl;
-    cout << ::a << endl;
-    cout << A::a << endl;
-    cout << B::a << endl;
+    // int a = 1;
+    // cout << a << endl;
+    // cout << ::a << endl;
+    // cout << A::a << endl;
+    // cout << B::a << endl;
+    // cout << b << endl;  // from using namespace A
 
-    cout << b << endl;  // from using namespace A
+    std::cout << "mySpace " << mySpace::endl.toString();
+    std::cout << "this is text: " << endl;
 
     return 0;
 }
